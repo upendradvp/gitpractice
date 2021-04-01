22d5467e182553e011984c545d596d751d2c4753
upendra <uuupendra6@gmail.com>
2021-04-02 00:05:19 +0530

    first commit

---
+++ powershell/availityset.ps1
@@ -0,0 +1,2 @@
+#Avaibilty Set
+New-AzureRmAvailabilitySet -ResourceGroupName "ResourceGroup11" -Name "AvailabilitySet03" -Location "West US"
---
+++ powershell/vpngateway.ps1
@@ -0,0 +1,8 @@
+#getting Existing Vnet
+#Create the gateway IP addressing configuration
+$vnet = Get-AzureRmVirtualNetwork -Name testvnet -ResourceGroupName testrg
+$subnet = Get-AzureRmVirtualNetworkSubnetConfig -Name 'GatewaySubnet' -VirtualNetwork $vnet
+$gwipconfig = New-AzureRmVirtualNetworkGatewayIpConfig -Name gwipconfig1 -SubnetId $subnet.Id -PublicIpAddressId $gwpip.Id
+
+#Create the virtual network gateway
+New-AzureRmVirtualNetworkGateway -Name vnetgw1 -ResourceGroupName testrg -Location 'West US' -IpConfigurations $gwipconfig -GatewayType Vpn -VpnType RouteBased -GatewaySku Standard
